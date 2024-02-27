d978a07b51ce1e271148b274f2dfe78afd9dff2b
Llixuma <fihlf@outlook.com>
2024-02-27 09:44:01 +0100

    Initial commit

---
+++ mediasort.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+# 22.01.24
+# By Llixuma
+# rectal use only
+# dont blame me if you break stuff with this thing
+# this tool is provided as is and without any warranties or promises of making your situation any better
+MFVERSION="2.24-beta"
+
+# store args in global variables
+OPT1=$1 # Directory
+OPT2=$2 # Option
+OPT3=$3 # Suboption
+OPT4=$4 # Output directory
+
+cd $OPT1 > /dev/null
+echo "Executing in $(pwd)"
+
+INDEXFILE="$(pwd)/.index" 2>/dev/null # dont throw file not found error if it doesnt exist
+
+ALBUMDEFINITION=10 # amount of files in a folder to consider it being an album
+
+##
+## in program fuctions
+##
+
+Album()
+{
+  ALBUMFILES=0 # number of files in a folder
+  FILEEXTENSION="" # grab the file extension for
+  FOLDERS=$(awk -F '/' -v OFS='/' 'NF-=2' $INDEXFILE | sort | uniq | sort -h)
+  FOLDERFILES=$(awk -F '/' -v OFS='/' 'NF-=2' $INDEXFILE | sort | uniq -c | sort -h | awk -F '/' '{print $1}')
+  for i in $FOLDERS
+  do
+    echo $i/
+    echo
+  done
+}
+
+##
+## Option functions
+##
+
+ShowUsage()
+{
+  echo "USAGE: mediasort.sh [DIRECTORY] [OPTION] [SUBOPTION]"
+  echo
+  echo "OPTIONS:"
+  echo "list - list occurences of the different file types"
+  echo "list [FILETYPE] - List files with filetype/specific"
+  echo "rebuild - Rebuild file index"
+  echo
+  echo "########### Danger zone! ###########"
+  echo "move [FILETYPE] [LOCATION] - Move files of file type to a different folder"
+  echo "DELETE [FILETYPE] - Delete all occurences of file type"
+  echo
+}
+
+CreateIndex()
+{
+  rm $INDEXFILE 2>/dev/null
+  touch $INDEXFILE
+  echo "Rebuilding..."
+  echo "This may create a LOT of disk cache"
+  echo "use 'echo 3 | sudo tee /proc/sys/vm/drop_caches' to free up the cache again"
+  trap "echo ;echo Terminated, deleting unfinished index...; rm $INDEXFILE; exit 1" INT
+  for i in $(ls)
+  do
+    find $(pwd)/$i -type f -exec file --mime-type {} \+ >> $INDEXFILE
+  done
+  echo done
+}
+
+List()
+{
+  if echo $FILETYPES | grep -q "$OPT3" && [ "$OPT3" != "" ]
+  then
+    grep "$OPT3" $INDEXFILE | more
+    exit 0
+  elif [ "$OPT3" == "" ]
+  then
+    awk -F ' ' '{print $NF}' $INDEXFILE | sort | uniq -c | sort -h | more
+  else
+    echo "couldn't find $OPT3"
+  fi
+}
+
+Move()
+{
+  if echo $FILETYPES | grep -q "$OPT3" && [ "$OPT3" != "" ]
+  then
+    grep "$OPT3" $INDEXFILE
+  else
+    echo "this its temporary"
+    exit 1
+  fi
+}
+
+Delete()
+{
+  TOBEDELETED=$(grep "$OPT3" $INDEXFILE | awk -F':' '{print $1}')
+  if echo $FILETYPES | grep -q "$OPT3" && [[ "$OPT3" != "" ]] && [[ "$TOBEDELETED" != "" ]]
+  then
+    echo "THIS WILL REMOVE EVERY FILE IN THE LIST BELOW"
+    echo
+    grep "$OPT3" $INDEXFILE | awk -F':' '{print $1}'
+    echo
+    echo
+    echo "if you are absolutely sure you want to remove these files type"
+    echo "                       "YES I AM SURE"                        "
+    echo
+    read FUCKMYSHITUP
+    if [[ $FUCKMYSHITUP = "YES I AM SURE" ]]
+    then
+      for delete in $TOBEDELETED
+      do
+        echo deleting $delete
+        rm $delete
+      done
+      echo done
+      CreateIndex
+    else
+      echo "Exiting"
+    fi
+  else
+    echo "No files to remove"
+    echo
+  fi
+}
+
+##
+## main part
+##
+
+echo mediafind.sh by Llixuma
+echo "Version $MFVERSION"
+
+if [ -f $INDEXFILE ]; then
+  echo
+else
+  echo No file index found
+  CreateIndex
+fi
+echo
+
+FILETYPES=$(awk -F ' ' '{print $NF}' $INDEXFILE | sort | uniq)
+
+
+case $OPT2 in
+  list) List ;;
+  rebuild) CreateIndex ;;
+  move) Move ;;
+  DELETE) Delete ;;
+  album) Album ;;
+  *) ShowUsage ;;
+esac
+
